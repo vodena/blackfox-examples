52474c12e01ee87fabfe5fcb478831170599f642
Dusan Stefanovic <dstefanovic@outlook.com>
2020-10-23 10:52:47 +0200

    novi pozivi optimizacija, novi logovi, dodati RF i XGB

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -3,24 +3,29 @@ var store = [{
         "excerpt":" ","categories": [],
         "tags": [],
         "url": "/blackfox-examples/examples/boston/",
-        "teaser":null},{
+        "teaser": null
+      },{
         "title": "Building energy consumption",
         "excerpt":" ","categories": [],
         "tags": [],
         "url": "/blackfox-examples/examples/building/",
-        "teaser":null},{
+        "teaser": null
+      },{
         "title": "Breast cancer classification",
         "excerpt":" ","categories": [],
         "tags": [],
         "url": "/blackfox-examples/examples/cancer/",
-        "teaser":null},{
+        "teaser": null
+      },{
         "title": "Customer churn",
         "excerpt":" ","categories": [],
         "tags": [],
         "url": "/blackfox-examples/examples/churn/",
-        "teaser":null},{
+        "teaser": null
+      },{
         "title": "First version",
         "excerpt":"First version of BlackFox python client is published on pip  ","categories": [],
         "tags": [],
         "url": "/blackfox-examples/first-version/",
-        "teaser":null}]
+        "teaser": null
+      }]
